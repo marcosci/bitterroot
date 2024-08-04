d1478deab6de8dc3edc0ade77ef96a3963991858
Marco Sciaini <marco.sciaini@posteo.net>
2024-08-04 17:11:38 +0200

    :feat: include tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,19 +3,16 @@
 	export let data;
 </script>
 
-<div>
-	<App {data} />
+<div class="h-screen overflow-hidden bg-slate-200">
+	<div class="flex items-center space-x-4 p-4">
+		<img src="/BitterrootLogoPNG.png" alt="Description of image" class="h-36" />
+		<h1>Bitterroot</h1>
+	</div>
+
+	<div class="h-full">
+		<App {data} />
+	</div>
 </div>
 
 <style>
-	:global(body) {
-		margin: 0;
-	}
-
-	div {
-		width: 100vw;
-		height: 100vh;
-		background: rgb(13, 19, 32);
-		background: linear-gradient(180deg, rgba(13, 19, 32, 1) 0%, rgba(8, 12, 21, 1) 100%);
-	}
 </style>
